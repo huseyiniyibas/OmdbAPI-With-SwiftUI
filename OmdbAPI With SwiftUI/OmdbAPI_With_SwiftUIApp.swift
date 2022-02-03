26bbbeee279999eab27cfd9e2e45359ee4cc7ae3
Hüseyin İyibaş <huseyin@mobilex.com.tr>
2022-02-03 11:38:13 +0300

    Initial Commit

---
+++ OmdbAPI With SwiftUI/OmdbAPI_With_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OmdbAPI_With_SwiftUIApp.swift
+//  OmdbAPI With SwiftUI
+//
+//  Created by Hüseyin İyibaş on 3.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct OmdbAPI_With_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
